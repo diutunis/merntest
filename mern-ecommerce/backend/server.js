09fe605d69000b43cb5784720ff0c1f3f18fa035
zeke <ezekielpaints@gmail.com>
2024-09-21 21:10:52 -0400

    deploys

--- mern-ecommerce/backend/server.js
+++ mern-ecommerce/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const productRoutes = require('./backend/routes/productRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 // Load environment variables
 dotenv.config();
