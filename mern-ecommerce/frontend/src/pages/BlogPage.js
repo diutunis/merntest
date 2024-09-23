c8c69acc9975b82c7d5b94ef7d4492c4c6d1ed9c
zeke <ezekielpaints@gmail.com>
2024-09-23 13:22:06 -0400

    deploys

--- mern-ecommerce/frontend/src/pages/BlogPage.js
+++ mern-ecommerce/frontend/src/pages/BlogPage.js
@@ -4,17 +4,14 @@ import './BlogPage.css'; // Optional for custom styles
 const BlogPage = () => {
     return (
         <div className="pdf-container">
-            <object
-                data=".\public\pdf-file.pdf"
-                type="application/pdf"
+            <iframe
+                src="./pdf-file.pdf"
+                title="Blog PDF"
                 width="100%"
                 height="1000px"
-            >
-                <p>
-                    It appears your browser doesn't support embedding PDFs. You can{' '}
-                    <a href="/path-to-your-pdf-file.pdf">download the PDF</a> instead.
-                </p>
-            </object>
+                frameBorder="0"
+                allowFullScreen
+            />
         </div>
     );
 };
