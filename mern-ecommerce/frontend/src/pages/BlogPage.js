02da6df66d98a014be183dca3c6bcbd707d05335
zeke <ezekielpaints@gmail.com>
2024-09-23 12:32:17 -0400

    deploys

--- mern-ecommerce/frontend/src/pages/BlogPage.js
+++ mern-ecommerce/frontend/src/pages/BlogPage.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import './BlogPage.css'; // Add some styles for the PDF container
 
 const BlogPage = () => {
-    return <h1>Welcome to Blog Page</h1>;
+    const [numPages, setNumPages] = useState(null);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
+    return (
+        <div className="pdf-container">
+            <Document
+                file="\public\pdf-file.pdf"  // Replace this with the actual path to your PDF
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                {Array.from(new Array(numPages), (el, index) => (
+                    <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                ))}
+            </Document>
+        </div>
+    );
 };
 
 export default BlogPage;
