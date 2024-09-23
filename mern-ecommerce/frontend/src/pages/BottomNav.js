1dec29939a3e08706bffb58a86f684d228b0fcea
zeke <ezekielpaints@gmail.com>
2024-09-23 16:18:30 -0400

    deploys

--- mern-ecommerce/frontend/src/pages/BottomNav.js
+++ mern-ecommerce/frontend/src/pages/BottomNav.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
+
 
 import './BottomNav.css'; // CSS for styling the navigation bar
 
