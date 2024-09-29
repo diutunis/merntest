d28700daaad51d1efa3e49c472bf7cf3a4d14d9d
zeke <ezekielpaints@gmail.com>
2024-09-29 15:57:37 -0400

    update portfolio

--- mern-ecommerce/frontend/src/pages/BlogPage.js
+++ mern-ecommerce/frontend/src/pages/BlogPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './BlogPage.css'; // Ensure you have appropriate styles for the PDF container
 import bookimage from './book.png'; // Adjust the path as needed
+import bookimage2 from './book2.png'; // Adjust the path as needed
+
 
 const BlogPage = () => {
 
     return (
         <>
             <section className="container" id="about">
-                <h2>You're so sweet, your cigarette smoke tastes like honey</h2>
+                <h2>Dont Forget to Pinch</h2>
                 <div className="container">
                     <div className="ticker-container">
                         <div className="ticker-tape">
-                            <div className="ticker-item">........</div>
+                            
                         </div>
                     </div>
                 </div>
@@ -25,9 +27,19 @@ const BlogPage = () => {
                 <img src={bookimage} alt="Example Artwork" className="artwork-image" />
             </section>
 
+<section id="artwork">
+                <p>
+                    {/* Artwork content */}
+                </p>
+                {/* Embedded Image */}
+                <img src={bookimage2} alt="Example Artwork" className="artwork-image" />
+            </section>
+
             <section id="print">
                 <h2>Good is he who gives and receives naught.</h2>
             </section>
+
+
         </>
     );
 };
