ab71c2079f27cd1a5480e1f87fd3160a973229ba
zeke <ezekielpaints@gmail.com>
2024-10-19 12:48:59 -0400

    audio comments

--- mern-ecommerce/frontend/src/pages/HomePage.js
+++ mern-ecommerce/frontend/src/pages/HomePage.js
@@ -208,9 +208,15 @@ const playAudio = async (audioURL) => {
     try {
         // Safari requires calling `resume()` to unlock the audio context.
         await audioContext.resume();
-
+	const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+        const source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(audioContext.destination);
         const audio = new Audio(audioURL);
         audio.crossOrigin = 'anonymous'; // Ensure CORS doesn't block audio
+	audio.playsInline = true; // Ensure playback in iOS Safari
         await audio.play();
     } catch (error) {
         console.error('Audio playback error:', error);
@@ -411,11 +417,14 @@ const createAudioElement = (audioURL) => {
     {drawing.comments?.map((comment, index) => (
         <div key={index} className="audio-comment">
             <button onClick={() => playAudio(comment.audioURL)}>
-                <FontAwesomeIcon icon={faPlay} /> Play Comment 
+                <FontAwesomeIcon icon={faPlay} /> Play Comment
             </button>
         </div>
     ))}
 </div>
+
+
+
 
                         </div>
                     </div>
